1b730f55071e3d0bde7a334dba16bf1bf02f07e5
ntno <ntno@users.noreply.github.com>
2022-11-09 00:21:10 -0500

    store distribution id in state/ssm (#2)

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
+
+output "portfolio_site_cloudfront_distribution_id" {
+  description = "portfoliio site's cloudfront distribution id"
+  value       = module.portfolio_site.content_cloudfront_distribution_info.id
+}
