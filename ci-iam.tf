9832443c2333708aae44e690a945ef97f2c95da8
ntno <ntno@users.noreply.github.com>
2022-12-22 00:51:41 -0500

    it seems like this should not be this difficult >.>

--- ci-iam.tf
+++ ci-iam.tf
@@ -16,7 +16,9 @@ resource "aws_iam_policy" "read_write_ntnonet_ci_policy" {
           "s3:PutObject",
           "s3:PutObjectAcl",
           "s3:GetObject",
-          "s3:GetEncryptionConfiguration"
+          "s3:GetEncryptionConfiguration",
+          "s3:DeleteObject",
+          "s3:DeleteObjectVersion"
         ]
         Effect = "Allow"
         Resource = [
