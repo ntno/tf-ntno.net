f754bb8b7d753a7a94d53333ad6572321934b6bb
ntno <ntno@users.noreply.github.com>
2022-12-22 00:39:09 -0500

    more access

--- ci-iam.tf
+++ ci-iam.tf
@@ -14,6 +14,7 @@ resource "aws_iam_policy" "read_write_ntnonet_ci_policy" {
       {
         Action = [
           "s3:PutObject",
+          "s3:PutObjectAcl",
           "s3:GetObject",
           "s3:GetEncryptionConfiguration"
         ]
