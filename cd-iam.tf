fb5256fbbafc20c8f3e843a614fc389c138a5b05
ntno <ntno@users.noreply.github.com>
2022-12-23 17:27:07 -0500

    cd user should be able to invalidate distribution

--- cd-iam.tf
+++ cd-iam.tf
@@ -3,7 +3,7 @@
 resource "aws_iam_policy" "read_write_ntnonet_cd_policy" {
   name        = format("ReadWrite_%s_S3", var.cd_user)
   path        = "/CustomerManaged/"
-  description = format("Allows read/write on %s S3 buckets", var.cd_user)
+  description = format("Allows read/write on %s S3 bucket", var.portfolio_domain_name)
   tags        = local.global_tags
 
   # Terraform's "jsonencode" function converts a
@@ -18,43 +18,34 @@ resource "aws_iam_policy" "read_write_ntnonet_cd_policy" {
           "s3:GetObject",
           "s3:GetEncryptionConfiguration",
           "s3:DeleteObject",
-          "s3:DeleteObjectVersion"
+          # "s3:DeleteObjectVersion"
         ]
         Effect = "Allow"
         Resource = [
-          format("arn:aws:s3:::%s*", var.cd_user),
-          format("arn:aws:s3:::%s*/*", var.cd_user)
+          format("arn:aws:s3:::%s", var.portfolio_domain_name),
+          format("arn:aws:s3:::%s/*", var.portfolio_domain_name)
         ]
       },
       {
         Action = [
           "s3:GetBucketPublicAccessBlock",
-          "s3:PutBucketPublicAccessBlock",
-          "s3:PutEncryptionConfiguration",
-          "s3:DeleteBucketWebsite",
           "s3:GetBucketWebsite",
-          "s3:PutBucketWebsite",
-          "s3:PutBucketPolicy",
-          "s3:CreateBucket",
           "s3:GetBucketAcl",
-          "s3:PutBucketAcl",
-          "s3:DeleteBucketPolicy",
-          "s3:DeleteBucket",
           "s3:GetBucketPolicy"
         ]
         Effect = "Allow"
         Resource = [
-          format("arn:aws:s3:::%s*", var.cd_user)
+          format("arn:aws:s3:::%s", var.portfolio_domain_name)
         ]
       }
     ]
   })
 }
 
-resource "aws_iam_policy" "read_write_ntnonet_cd_stack_policy" {
-  name        = format("ReadWrite_%s_CloudformationStack", var.cd_user)
+resource "aws_iam_policy" "invalidate_cloudfront_cd_policy" {
+  name        = format("InvalidateCloudfrontDistribution_%s", var.cd_user)
   path        = "/CustomerManaged/"
-  description = format("Allows read/write on %s Cloudformation Stacks", var.cd_user)
+  description = format("Allows invalidate on %s Cloudfront Distribution", var.portfolio_domain_name)
   tags        = local.global_tags
 
   # Terraform's "jsonencode" function converts a
@@ -64,29 +55,23 @@ resource "aws_iam_policy" "read_write_ntnonet_cd_stack_policy" {
     Statement = [
       {
         Action = [
-          "cloudformation:RollbackStack",
-          "cloudformation:CreateStack",
-          "cloudformation:Describe*",
-          "cloudformation:DeleteStack",
-          "cloudformation:ListStackResources"
+          "cloudfront:CreateInvalidation"
         ]
         Effect = "Allow"
         Resource = [
-          format("arn:aws:cloudformation:*:%s:stack/%s*/*", data.aws_caller_identity.current.account_id, var.cd_user)
+          format("arn:aws:cloudfront::%s:distribution/%s", data.aws_caller_identity.current.account_id, module.portfolio_site.content_cloudfront_distribution_info.id)
         ]
       }
     ]
   })
 }
 
-
-
-resource "aws_iam_user_policy_attachment" "read_write_artifacts_policy_attachment" {
+resource "aws_iam_user_policy_attachment" "read_write_artifacts_cd_policy_attachment" {
   user       = var.cd_user
   policy_arn = aws_iam_policy.read_write_artifacts_policy.arn
 }
 
-resource "aws_iam_user_policy_attachment" "read_write_ssm_policy_attachment" {
+resource "aws_iam_user_policy_attachment" "read_write_ssm_cd_policy_attachment" {
   user       = var.cd_user
   policy_arn = aws_iam_policy.read_write_ssm_policy.arn
 }
@@ -96,7 +81,7 @@ resource "aws_iam_user_policy_attachment" "read_write_ntnonet_cd_policy_attachme
   policy_arn = aws_iam_policy.read_write_ntnonet_cd_policy.arn
 }
 
-resource "aws_iam_user_policy_attachment" "read_write_ntnonet_cd_stack_policy_attachment" {
+resource "aws_iam_user_policy_attachment" "invalidate_cloudfront_cd_policy_attachment" {
   user       = var.cd_user
-  policy_arn = aws_iam_policy.read_write_ntnonet_cd_stack_policy.arn
+  policy_arn = aws_iam_policy.invalidate_cloudfront_cd_policy.arn
 }
