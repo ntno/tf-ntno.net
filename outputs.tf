e2d9fc681840b3089299e054c2bd046f61517f2c
ntno <ntno@users.noreply.github.com>
2022-12-26 01:02:36 -0500

    add role names to output

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,13 @@ output "portfolio_site_cloudfront_distribution_id" {
   description = "portfoliio site's cloudfront distribution id"
   value       = module.portfolio_site.content_cloudfront_distribution_info.id
 }
+
+output "ci_role_name" {
+  description = "CI IAM Role Name"
+  value       = module.portfolio_site_ci_cd.ci_role_name
+}
+
+output "cd_role_name" {
+  description = "CD IAM Role Name"
+  value       = module.portfolio_site_ci_cd.cd_role_name
+}
